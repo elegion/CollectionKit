101cdec6e325590ae23aca6e28ee7bf103aff7ab
viktor.volkov <viktor.volkov@e-legion.com>
2020-10-01 15:03:50 +0300

    Reorganize folder structure

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IVCollectionKit",
+    products: [
+        .library(
+            name: "IVCollectionKit",
+            targets: ["IVCollectionKit"]),
+    ],
+    targets: [
+        .target(
+            name: "IVCollectionKit",
+            dependencies: []),
+        .testTarget(
+            name: "IVCollectionKitTests",
+            dependencies: ["IVCollectionKit"]),
+    ]
+)
